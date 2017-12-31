387eaef6089fd08449a8b50919ba8067e54a2706
droneRL2020 <seoho0527@gmail.com>
2017-12-31 20:43:24 +0900

    AI-agent SNU-BDI courses

---
+++ 2nd semester/Deep Learning_Prof. Kim Gunhee/실습/train.py
@@ -0,0 +1,54 @@
+from tensorflow import flags
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("./data", one_hot=True)
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer("batch_size", 128, "number of batch size. default 128.")
+flags.DEFINE_float("learning_rate", 0.01, "initial learning rate.")
+flags.DEFINE_integer("max_steps", 10000, "max steps to train.")
+
+batch_size = FLAGS.batch_size
+learning_rate = FLAGS.learning_rate
+max_step = FLAGS.max_steps
+
+
+
+model_inputs = tf.placeholder(dtype = tf.float32, shape= [None, 784])
+labels = tf.placeholder(dtype=tf.float32, shape=[None, 10])
+
+w = tf.Variable(tf.random_normal(shape=[784, 10]))
+b = tf.Variable(tf.random_normal(shape=[10]))
+
+logits = tf.matmul(model_inputs, w) + b
+predictions = tf.nn.softmax(logits)
+
+loss = tf.losses.softmax_cross_entropy(
+    onehot_labels=labels,
+    logits = predictions)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(loss)
+tf.summary.scalar("loss", loss)
+tf.summary.histogram("W", w)
+tf.summary.histogram("b", b)
+merge_op = tf.summary.merge_all()
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    summary_writer = tf.summary.FileWriter("./logs", sess.graph)
+    for step in range(max_step):
+        batch_images, batch_labels = mnist.train.next_batch(batch_size)
+        feed = {model_inputs: batch_images, labels: batch_labels}
+        _, loss_val = sess.run([train_op, loss], feed_dict=feed)
+        if step % 10 == 0:
+            summary = sess.run(merge_op, feed_dict=feed)
+            summary_writer.add_summary(summary, step)py
+            
+            
+
+
+
+
